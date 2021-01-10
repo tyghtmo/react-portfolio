3c6ba93ae8c6bae031972c77860e15f0815e6500
Tyson Wright <tyson.w259@gmail.com>
2021-01-10 14:08:01 +1100

    Added title and icon to contact section

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 class Contact extends Component {
   render() {
@@ -11,6 +13,14 @@ class Contact extends Component {
       //   Add HTML here
       <section id="contact">
         <Container>
+          <Row>
+            <Col sm={3} className="text-right mb-4">
+              <FontAwesomeIcon icon={faEnvelope} size="4x" className="icon" />
+            </Col>
+            <Col>
+              <h1 className="thin">Contact</h1>
+            </Col>
+          </Row>
           <Form>
             <Form.Group as={Row} controlId="contactFormName">
               <Form.Label column="true" sm={3} className="text-right">
